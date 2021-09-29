40b28939578413fcfe7a46019a8fe7adb19fbfea
icewyrmling <stojanovic.sofi@gmail.com>
2021-09-29 14:24:38 +0200

    added city picker

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,43 +1,84 @@
 @extends('layouts.app')
+<?php
+$filename = 'gradovi.txt';
+$eachlines = file($filename, FILE_IGNORE_NEW_LINES);
+?>
+<script>
+    var count;
+    var cities;
+
+    window.onload = function(){
+        document.getElementById("textarea1").value = "";
+        count = 0;
+        cities = [];
+    }
+
+    function add_to_list(){
+        var select = document.getElementById('select1');
+        var value = select.options[select.selectedIndex].value;
+        console.log(value);
+        if(!cities.length){
+            cities[count++] = value
+            textarea1.value += value;
+        }
+        else if (cities.length < 9){
+            if(!cities.includes(value)){
+                cities[count++] = value;
+                textarea1.value += '\n' + value;
+            }
+        }
+        else if(cities.length == 9){
+            if(!cities.includes(value)){
+                cities[count] = value;
+                textarea1.value += '\n' + value;
+                let button = document.getElementById("addToListButton");
+                button.disabled = true;
+            }
+        }
+    }
+</script>
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-               <!-- <div class="card-header">{{ __('Dashboard') }}</div> -->
+               <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="container">
-                    <div class="row gx-lg-1">
-                        <div class="col-lg-6 col-xxl-6 mb-10">
+                    <div class="row gx-lg-4">
+                        <div class="col-lg-10 col-xxl-6 mb">
                             <div class="card bg-light border-0 h-100">
                                 <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
                                     <h2 class="fs-4 fw-bold">City picker:</h2>
-                                    <div class="dropdown">
-                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Pick a city
-                                        </button>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            <a class="dropdown-item" href="#">city1</a>
-                                            <a class="dropdown-item" href="#">city2</a>
+                                    <div class="select">
+                                    <select class="form-select" id="select1" aria-label="city_picker">
+                                        <?php foreach($eachlines as $lines){echo "<option value='".$lines."'>$lines</option>";}?>
+                                    </select>
+                                        <div>
+                                            <button class="btn btn-primary" type="button" id="addToListButton" onclick="add_to_list()">
+                                                Add
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-2 col-xxl-6 mb-10">
+                        <div class="col-lg-10 col-xxl-6 mb">
                             <div class="card bg-light border-0 h-100">
                                 <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
                                     
                                     <h2 class="fs-4 fw-bold">Chosen cities:</h2>
+                                    <div class="form-group">
+                                       <textarea  id="textarea1" rows="10"></textarea>
+                                    </div>
                                     
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-
+                
                <!--  <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,19 +24,7 @@
     @section('content')
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <!-- @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif -->
+            
             
 
             <header class="py-5">
@@ -45,7 +33,6 @@
                     <div class="m-4 m-lg-5">
                         <h1 class="display-5 fw-bold">A warm welcome!</h1>
                         <p class="fs-4">Would you like to get personalized weather forecast? Sign up today!</p>
-                        <!-- <a class="btn btn-primary btn-lg" href="#!">Call to action</a> -->
                     </div>
                 </div>
             </div>
@@ -90,10 +77,7 @@
         <footer class="py-5 bg-dark">
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy;icewyrmling 2021</p></div>
         </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+        
         </div>
     </body>
     @endsection
