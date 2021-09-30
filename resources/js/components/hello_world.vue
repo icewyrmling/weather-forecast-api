bca1901716d1d267097bce402e5a1d20020385e5
icewyrmling <stojanovic.sofi@gmail.com>
2021-09-30 11:04:12 +0200

    rewrote some parts of code in VueJS

---
+++ resources/js/components/hello_world.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+    new Vue({
+        el: "#app",
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        data() {
+            return {
+                msg: "Hello world",
+            }
+        }
+    })
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 $filename = 'gradovi.txt';
 $eachlines = file($filename, FILE_IGNORE_NEW_LINES);
 ?>
-<script>
+<!-- <script>
     var count;
     var cities;
 
@@ -36,7 +36,7 @@ $eachlines = file($filename, FILE_IGNORE_NEW_LINES);
             }
         }
     }
-</script>
+</script> -->
 
 @section('content')
 <div class="container">
@@ -45,23 +45,30 @@ $eachlines = file($filename, FILE_IGNORE_NEW_LINES);
             <div class="card">
                <div class="card-header">{{ __('Dashboard') }}</div>
 
+
+                
+                
+                <!-- 
                 <div class="container">
                     <div class="row gx-lg-4">
                         <div class="col-lg-10 col-xxl-6 mb">
                             <div class="card bg-light border-0 h-100">
                                 <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-                                    <h2 class="fs-4 fw-bold">City picker:</h2>
-                                    <div class="select">
+                                    <h2 class="fs-4 fw-bold">City picker:</h2> -->
+                                        <div>
+                                            <example-component></example-component>
+                                        </div>
+                                    <!-- <div class="select">
                                     <select class="form-select" id="select1" aria-label="city_picker">
                                         <?php foreach($eachlines as $lines){echo "<option value='".$lines."'>$lines</option>";}?>
-                                    </select>
-                                        <div>
+                                    </select>-->
+                                        <!-- <div>
                                             <button class="btn btn-primary" type="button" id="addToListButton" onclick="add_to_list()">
                                                 Add
                                             </button>
-                                        </div>
-                                    </div>
-                                </div>
+                                        
+                                    </div>  -->
+                               <!--  </div>
                             </div>
                         </div>
                         <div class="col-lg-10 col-xxl-6 mb">
@@ -77,7 +84,7 @@ $eachlines = file($filename, FILE_IGNORE_NEW_LINES);
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
                 
                <!--  <div class="card-body">
                     @if (session('status'))
